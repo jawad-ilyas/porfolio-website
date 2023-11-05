49ef58db8ee046f27b37acc84beaa286b1710e42
jawad mughal <taleem35@gmail.com>
2023-11-05 23:09:52 +0500

    new changing

--- portfolio/src/Components/Contact/Contact.jsx
+++ portfolio/src/Components/Contact/Contact.jsx
@@ -1,111 +1,75 @@
-import React, { useState } from 'react';
+import React from 'react'
+import { useForm } from "react-hook-form"
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const onSubmit = (data) => console.log(data)
 
-    // Submit the form data to Netlify using fetch
-    fetch('/.netlify/functions/submitForm', {
-      method: 'POST',
-      body: JSON.stringify(formData),
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log('Form submitted successfully');
-        } else {
-          console.error('Form submission failed');
-        }
-      })
-      .catch((error) => {
-        console.error('An error occurred while submitting the form', error);
-      });
-  };
+    return (
+        <div className='bg-white dark:bg-black' id="contact">
 
-  return (
-    <div>
-      <h2>Contact Us</h2>
-      <form
-        name="contact"
-        method="POST"
-        data-netlify="true"
-        onSubmit={handleSubmit}
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-          />
+            <div className='md:max-w-screen-md max-w-screen-sm mx-auto bg-white dark:bg-black md:py-20  p-6'>
+
+                <div className='text-center'>
+                    <h4 className='text-xl font-medium'>Get In Touch</h4>
+                    <h1 className='text-3xl font-semibold pt-2'>Contact Me</h1>
+                    <p className='py-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, odit.
+                    </p>
+                </div>
+                <div>
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div class="grid gap-6 mb-6 md:grid-cols-2">
+                            <div>
+                                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+                                <input {...register("firstName")} type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" />
+                            </div>
+                            <div>
+                                <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
+                                <input  {...register("lastName")} type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" />
+                            </div>
+                            <div>
+                                <label for="company" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company</label>
+                                <input type="text"  {...register("company")} id="company" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Expo Tech Solution " />
+                            </div>
+                            <div>
+                                <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone number</label>
+                                <input type="tel"  {...register("phone")} id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" />
+                                {/* pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" */}
+                            </div>
+
+
+                        </div>
+
+                        <div class="mb-6">
+                            <label for="website" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Website URL</label>
+                            <input   {...register("website")} type="url" id="website" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="jawadmughal.netlify.com" />
+                        </div>
+                        <div class="mb-6">
+                            <label for="email"  {...register("email")} class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
+                            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" />
+                        </div>
+
+                        <div class="mb-6">
+                            <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Message</label>
+                            <textarea  {...register("subject")} className='w-full  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500  p-2.5 dark:bg-gray-700 dark:border-gray-600  dark:placeholder-gray-400 dark:text-white ' placeholder="Enter Your Message"></textarea>
+                        </div>
+
+
+                        <button type="submit" class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">Submit</button>
+                    </form>
+
+                </div>
+            </div>
         </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
-  );
-};
+    )
+}
 
-export default Contact;
-
-// Netlify serverless function
-exports.handler = async (event) => {
-  if (event.httpMethod !== 'POST') {
-    return {
-      statusCode: 405,
-      body: 'Method Not Allowed',
-    };
-  }
-
-  try {
-    const { name, email, message } = JSON.parse(event.body);
-
-    // You can now process the form data as needed, e.g., send an email, save to a database, etc.
-
-    // Replace this with your actual form handling logic
-
-    return {
-      statusCode: 200,
-      body: 'Form submitted successfully',
-    };
-  } catch (error) {
-    console.error('Form submission error:', error);
-    return {
-      statusCode: 500,
-      body: 'Form submission failed',
-    };
-  }
-};
+export default Contact
